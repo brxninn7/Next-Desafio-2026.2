e7a63701a2ea2e87df7b0d9d86ca804bbea3117b
Bruno <brunoar780@gmail.com>
2026-08-08 16:44:04 -0300

    feat: navbar para desktop feita

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,38 +1,50 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Search, ShoppingCart} from "lucide-react";
+import {
+  Avatar,
+  AvatarBadge,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 
 export function Header() {
     return (
-        <header className="flex justify-between">
-            <div id="left-side" className="flex">
-                <Image src="/logo.png" alt="" width={40} height={40}/>
+        <header className="flex justify-between items-center p-4">
+            <div id="left-side" className="flex gap-5">
 
-                <div id="links" className="flex">
-                    <Link href="/">
+                <Link href="/">
+                    <Image src="/logo.png" alt="" width={50} height={50}/>
+                </Link>
+                <div id="links" className="flex text-[#F5F5F5] gap-5 items-center text-lg">
+                    <Link href="/" className="opacity-50 hover:opacity-100 transition-opacity">
                         Inicio
                     </Link>
 
-                    <Link href="/jogos">
+                    <Link href="/jogos" className="opacity-50 hover:opacity-100 transition-opacity">
                         Jogos
                     </Link>
 
-                    <Link href="/contato">
+                    <Link href="/contato" className="opacity-50 hover:opacity-100 transition-opacity">
                         Contato
                     </Link>
                 </div>
             </div>
 
-            <div id="right-side" className="flex">
-                <div className="flex">
-                    <Search />
+            <div id="right-side" className="flex text-[#F5F5F5] text-lg gap-5 items-center">
+                <div className="flex gap-3 items-center">
+                    <Search className="opacity-50 hover:opacity-100 transition-opacity" />
 
-                    <Link href="/carrinho">
+                    <Link href="/carrinho" className="opacity-50 hover:opacity-100 transition-opacity">
                         <ShoppingCart />
                     </Link>
-
-                    
                 </div>
+
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                    <AvatarBadge className="bg-green-600 dark:bg-green-800" />
+                </Avatar>
             </div>
         </header>
     );
